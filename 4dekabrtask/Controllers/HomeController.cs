c6fbbd7ca6902cf8cf8fd1ee64c79ccd6a7d8516
Sam1rrr <samiroruclu10@gmail.com>
2023-12-05 13:43:30 +0400

    task

---
+++ 4dekabrtask/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using _4dekabrtask.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _4dekabrtask.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        } 
+    }
+}
---
+++ 4dekabrtask/Models/Slider.cs
@@ -0,0 +1,9 @@
+﻿namespace _4dekabrtask.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
